6f739a73f7db137e974650b5110a8b8091850bcc
Martin Bradley <mbrad@home.com>
2018-08-31 20:09:50 +0100

    Added rest test as a Junit test

---
+++ src/test/java/martinbradley/hospital/rest/RestClientTest.java
@@ -0,0 +1,57 @@
+package martinbradley.hospital.rest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class RestClientTest
+{
+    @BeforeEach
+    public void beforeEachTest()
+    {
+    }
+
+    @Test
+    public void callGet()
+    {
+        int id = 1;
+        Client client = ClientBuilder.newClient();
+
+        String url = String.format("http://localhost:8080/firstcup/rest/hospital/patient/%d",id);
+        
+        String response = client.target(url)
+                                .request()
+                                .get(String.class);
+        assertNotNull(response);
+    }
+
+    @Test
+    public void callPost()
+    {
+       String forename = "martin";
+       String surname = "bradley";
+
+       String xml = String.format(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
+                "<patient><id>1</id><forename>%s</forename><surname>%s</surname>" +
+                "<male>true</male></patient>", forename, surname);
+
+        Client client = ClientBuilder.newClient();
+
+        String url = "http://localhost:8080/firstcup/rest/hospital/patient/";
+        
+        String response = client.target(url)
+                                .request()
+                                .post(Entity.entity(xml, MediaType.APPLICATION_XML), String.class);
+    }
+
+}
